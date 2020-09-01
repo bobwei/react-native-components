ea2cd23f0a84cac39c0cd91d4f9f7d2fcf97223d
Bob Wei <bob61016@gmail.com>
2020-09-01 23:36:33 +0800

    Init

---
+++ App.js
@@ -0,0 +1,24 @@
+import 'react-native-gesture-handler';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SplashScreen from 'react-native-splash-screen';
+
+import Home from './src/screens/Home';
+
+const Stack = createStackNavigator();
+
+const Comp = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Comp;
