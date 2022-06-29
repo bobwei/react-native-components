5ea2eac9676d69e17718e88bc1e2840818bf054d
Bob Wei <bob61016@gmail.com>
2022-06-29 14:17:00 +0800

    Add ignoreLogs

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+LogBox.ignoreLogs([
+  "ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from 'deprecated-react-native-prop-types'.",
+  'NativeBase: The contrast ratio of',
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
