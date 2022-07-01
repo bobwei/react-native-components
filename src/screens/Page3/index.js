ebbcf4518fa58be0c31add616048de6378a17979
Bob Wei <bob61016@gmail.com>
2022-07-01 15:08:10 +0800

    Add count down

--- src/screens/Page3/index.js
+++ src/screens/Page3/index.js
@@ -5,9 +5,11 @@ import { Button } from 'react-native-elements';
 
 import styles from './styles';
 import video from './test_video.mp4';
+import CountDown from '../../components/CountDown';
 
 const Comp = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isCountDownVisible, setIsCountDownVisible] = useState(false);
   const dimensions = useWindowDimensions();
   const videoSize = {
     width: dimensions.width,
@@ -22,7 +24,21 @@ const Comp = () => {
           style={styles.videoStyle}
           resizeMode="contain"
           controls
+          onEnd={() => {
+            setIsCountDownVisible(true);
+          }}
         />
+        {isCountDownVisible && (
+          <CountDown
+            duration={5}
+            onComplete={() => {
+              alert('onComplete');
+            }}
+            onCancel={() => {
+              alert('onCancel');
+            }}
+          />
+        )}
       </View>
       <Button
         style={styles.button}
