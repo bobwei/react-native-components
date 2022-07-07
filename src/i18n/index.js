8dad4047f70c057c293856ef7abf4666d353f800
Bob Wei <bob61016@gmail.com>
2022-07-07 16:30:56 +0800

    Rm unnecessary langs

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,38 +3,8 @@ import { initReactI18next } from 'react-i18next';
 import { Platform, NativeModules } from 'react-native';
 
 const resources = {
-  'zh-Hant': {
-    translation: require('./langs/zh-Hant.json'),
-  },
-  'zh-Hans': {
-    translation: require('./langs/zh-Hans.json'),
-  },
   en: {
     translation: require('./langs/en.json'),
-  },
-  vi: {
-    translation: require('./langs/vi.json'),
-  },
-  km: {
-    translation: require('./langs/km.json'),
-  },
-  th: {
-    translation: require('./langs/th.json'),
-  },
-  ar: {
-    translation: require('./langs/ar.json'),
-  },
-  ja: {
-    translation: require('./langs/ja.json'),
-  },
-  ko: {
-    translation: require('./langs/ko.json'),
-  },
-  pl: {
-    translation: require('./langs/pl.json'),
-  },
-  ru: {
-    translation: require('./langs/ru.json'),
   },
 };
 
